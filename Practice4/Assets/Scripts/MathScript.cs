eeddda237a8f8cc8695013cc2eab7b75e3ca40bd
Ertal <109916187+chinetov-e@users.noreply.github.com>
2025-04-10 19:01:32 +0600

    CompareCanvasCommit

--- Practice4/Assets/Scripts/MathScript.cs
+++ Practice4/Assets/Scripts/MathScript.cs
@@ -7,32 +7,66 @@ using System;
 
 public class MathScript : MonoBehaviour
 {
-    [SerializeField] private InputField firstDigit;
-    [SerializeField] private InputField secondDigit;
-    [SerializeField] private Text result;
-    private int sum = 0;
+    [SerializeField] private InputField firstDigitCal;
+    [SerializeField] private InputField secondDigitCal;
+    [SerializeField] private InputField firstDigitCom;
+    [SerializeField] private InputField secondDigitCom;
+    [SerializeField] private Text calculatorResult;
+    [SerializeField] private Text compareResult;
+    [SerializeField] private GameObject firstScreen;
+    [SerializeField] private GameObject secondScreen;
+    private GameObject currentScreen;
+
+    private void Start()
+    {
+        firstScreen.SetActive(true);
+        currentScreen = firstScreen;
+
+    }
+
+    public void ChangeState(GameObject state)
+    {
+        if(currentScreen != null)
+        {
+            currentScreen.SetActive(false);
+            state.SetActive(true);
+            currentScreen = state;
+        }
+    }
 
     public void Sum ()
     {
-        sum = Convert.ToInt32(firstDigit.text) + Convert.ToInt32(secondDigit.text);
-        result.text = sum.ToString();
+        calculatorResult.text = (Convert.ToDouble(firstDigitCal.text) + Convert.ToDouble(secondDigitCal.text)).ToString();
     }
 
     public void Subtraction()
     {
-        sum = Convert.ToInt32(firstDigit.text) - Convert.ToInt32(secondDigit.text);
-        result.text = sum.ToString();
+        calculatorResult.text = (Convert.ToDouble(firstDigitCal.text) - Convert.ToDouble(secondDigitCal.text)).ToString();
     }
 
     public void Division()
     { 
-        sum = Convert.ToInt32(firstDigit.text) / Convert.ToInt32(secondDigit.text);
-        result.text = sum.ToString();
+        calculatorResult.text = (Convert.ToDouble(firstDigitCal.text) / Convert.ToDouble(secondDigitCal.text)).ToString();
     }
 
     public void Multiplication()
     {
-        sum = Convert.ToInt32(firstDigit.text) * Convert.ToInt32(secondDigit.text);
-        result.text = sum.ToString();
+        calculatorResult.text = (Convert.ToDouble(firstDigitCal.text) * Convert.ToDouble(secondDigitCal.text)).ToString();
+    }
+
+    public void Compare()
+    {
+        if (Convert.ToDouble(firstDigitCom.text) == Convert.ToDouble(secondDigitCom.text))
+        {
+            compareResult.text = "Равны";
+        }
+        else if(Convert.ToDouble(firstDigitCom.text) <= Convert.ToDouble(secondDigitCom.text))
+        {
+            compareResult.text = secondDigitCom.text;
+        }
+        else if (Convert.ToDouble(firstDigitCom.text) >= Convert.ToDouble(secondDigitCom.text))
+        {
+              compareResult.text = firstDigitCom.text;
+        }
     }
 }
